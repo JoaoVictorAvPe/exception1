cc53b2865fc48747004b4e061a8a7bf403319284
JoaoVictorAvPe <124708663+JoaoVictorAvPe@users.noreply.github.com>
2023-02-10 17:39:25 -0300

    VersãoBoa
    Versão final com tratamento de exceções feitos da maneira apropriada, conforme visto na aula

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -2,48 +2,53 @@ package aplication;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 import entitites.Reservation;
+import exceptions.DomainException;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		Reservation reservation = null;
-		
-		System.out.print("Room number: ");
-		int roomNumber = sc.nextInt();
-		System.out.print("Check-in date (dd/mm/yyyy): ");
-		LocalDate checkin = LocalDate.parse(sc.next(), fmt);
-		System.out.print("Check-out date (dd/mm/yyyy): ");
-		LocalDate checkout = LocalDate.parse(sc.next(), fmt);
-		if (checkout.isAfter(checkin)) {
-			reservation = new Reservation(roomNumber, checkin, checkout);
+
+		try {
+			System.out.print("Room number: ");
+			int roomNumber = sc.nextInt();
+			System.out.print("Check-in date (dd/mm/yyyy): ");
+			LocalDate checkin = LocalDate.parse(sc.next(), fmt);
+			System.out.print("Check-out date (dd/mm/yyyy): ");
+			LocalDate checkout = LocalDate.parse(sc.next(), fmt);
+
+			Reservation reservation = new Reservation(roomNumber, checkin, checkout);
 			System.out.println(reservation);
-		} else {
-			System.out.println("Error in reservation: Check-out date must be after check-in date");
-		}
-		
-		System.out.println("\nEnter data to update the reservation: ");
-		System.out.print("Check-in date (dd/mm/yyyy): ");
-		checkin = LocalDate.parse(sc.next(), fmt);
-		System.out.print("Check-out date (dd/mm/yyyy): ");
-		checkout = LocalDate.parse(sc.next(), fmt);
-		String error = reservation.updateDates(checkin, checkout);
-		if (error != null) {
-			System.out.println(error);
-		} else {
+
+			System.out.println("\nEnter data to update the reservation: ");
+			System.out.print("Check-in date (dd/mm/yyyy): ");
+			checkin = LocalDate.parse(sc.next(), fmt);
+			System.out.print("Check-out date (dd/mm/yyyy): ");
+			checkout = LocalDate.parse(sc.next(), fmt);
+
+			reservation.updateDates(checkin, checkout);
 			System.out.println(reservation);
-		}
+		} 
 		
+		catch (DomainException e) {
+			System.out.println(e.getMessage());
+		}
+		catch (InputMismatchException e) {
+			System.out.println("Only numbers are accepted");
+		}
+		catch (RuntimeException e) {
+			System.out.println("error");
+		}
 		
 		sc.close();
 	}
-	
+
 	public static DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
 }
